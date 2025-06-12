55c851ba40a388eb7d63869531577ed0eb81ee12
Humble689 <marktravis689@gmail.com>
2025-06-12 16:16:26 +0300

    100 line of code😂😂😂🥲

--- money_count-machine.c
+++ money_count-machine.c
@@ -86,6 +86,15 @@ void loop() {
             lcd.print("count:");
             lcd.print(billCount);
             lcd.print(" ");
+            lcd.print(currencies[currencyIndex]);
+
+
+        lcd.setCursor(0,0);
+        lcd.print("Total:");
+        lcd.print(billCount * selectedDenom);
+        lcd.print("    ");
         }
     }
+
+    //Reset button; shortpress to reset, long press to wipe EEPROM
 }
